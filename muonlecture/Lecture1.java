eab802e1e92f065cc0dcbe7e809cd4822557b11a
MXTarcelo <25856757+MXTarcelo@users.noreply.github.com>
2022-09-08 08:14:44 +0800

    Add files via upload

---
+++ muonlecture/Lecture1.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package muonlecture;
+
+/**
+ *
+ * @author staff
+ */
+public class Lecture1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int random = (int) Math.floor(Math.random()*10) + 1;
+        System.out.println(random);
+    }
+    
+}
